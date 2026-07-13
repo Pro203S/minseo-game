f43627a483970b2734af343f132fc1149753263e
prosirea2501@gmail.com <prosirea2501@gmail.com>
2026-07-13 16:07:01 +0900

    feat(index): 메인 페이지 완성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,31 @@
 "use client";
 
+import InOutAnimation from '@/components/InOutAnimation';
 import css from './page.module.css';
+import Link from 'next/link';
 
 export default function Page() {
     return (
-        <div>
-
+        <div className={css.container}>
+            <div className={css.texts}>
+                <InOutAnimation className={css.text} animate delay={500}>
+                    민서가
+                </InOutAnimation>
+                <InOutAnimation className={css.text} animate delay={1500}>
+                    좋아하는
+                </InOutAnimation>
+                <InOutAnimation className={css.text} animate delay={2500}>
+                    랜덤게임
+                </InOutAnimation>
+            </div>
+            <InOutAnimation animate delay={3500} className={css.buttons}>
+                <Link href="/search">
+                    <span>게임 찾아보기</span>
+                </Link>
+                <Link href="/random">
+                    <span>I&rsquo;m feeling lucky</span>
+                </Link>
+            </InOutAnimation>
         </div>
     );
 }
